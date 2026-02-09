ea4bd029912d1199a6e9976d8b89a4c7a2ecdff8
Massamba-commits <paparafinha20@gmail.com>
2026-02-09 19:52:20 +0000

    suppression payment des fichiers Python reste uniquement les fichiers HTML

--- payment/orange.py
+++
@@ -1,7 +0,0 @@
-# providers/orange.py
-from providers.paytech_provider import PayTechProvider
-
-class OrangeMoney:
-    def pay(self, item_name, amount, ref_command, success_url, cancel_url, ipn_url):
-        provider = PayTechProvider()
-        return provider.pay(item_name, amount, ref_command, success_url, cancel_url, ipn_url, "Orange Money")
--- payment/wave.py
+++
@@ -1,7 +0,0 @@
-# providers/wave.py
-from paytech_provider import PayTechProvider
-
-class WaveMoney:
-    def pay(self, item_name, amount, ref_command, success_url, cancel_url, ipn_url):
-        provider = PayTechProvider()
-        return provider.pay(item_name, amount, ref_command, success_url, cancel_url, ipn_url, "Wave")
